9f5249d8ed628ddcd9d90671e3d549319400468c
Malge <malge@example.com>
2024-06-18 15:04:47 +0530

    roxiler commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Transactions from './components/Transactions';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Transactions />
+    </div>
+  );
+}
+
+export default App;
